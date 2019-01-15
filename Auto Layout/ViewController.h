0b019dd7592e41a0372d27fe9f87143ed24046d4
28cmm <joshuafang1998@gmail.com>
2019-01-15 14:57:57 -0800

    Initial Commit

---
+++ Auto Layout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Auto Layout
+//
+//  Created by Yilei Huang on 2019-01-15.
+//  Copyright © 2019 Joshua Fanng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
